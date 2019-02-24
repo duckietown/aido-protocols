147e3cfc3a5e234396bf23af32007baa55a0bc7c
Andrea Censi <acensi@ethz.ch>
2019-02-24 06:22:55 -0800

    fix

--- src/aido_nodes/language.py
+++ src/aido_nodes/language.py
@@ -1,109 +1,67 @@
-from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 from typing import Tuple, Dict
 
 # Events
 
-ChannelName = str  # NewType('ChannelName', str)
+ChannelName = str
 
 
 class Event:
     pass
 
+
 @dataclass
 class InputReceived(Event):
     channel: ChannelName
+
 
 @dataclass
 class OutputProduced(Event):
     channel: ChannelName
 
-class NoMoreEvents(Event):
-    pass
-
-@dataclass
-class InputClosed(Event):
-    channel: ChannelName
-
-@dataclass
-class OutputClosed(Event):
-    channel: ChannelName
-
-
-class LanguageNotRecognized(Exception):
-    pass
-
 
 # Language over events
 
-@dataclass
-class Language(metaclass=ABCMeta):
-
-    def reset(self):
-        pass
-
-    @abstractmethod
-    def push(self, x: Event):
-        """ Does nothing or raises LanguageNotRecognized """
+class Language:
+    pass
 
 
 @dataclass
 class ExpectInputReceived(Language):
     channel: ChannelName
 
-    def push(self, x: Event):
-        pass  # XXX
-
 
 @dataclass
 class ExpectOutputProduced(Language):
     channel: ChannelName
-
-    def push(self, x: Event):
-        pass  # XXX
 
 
 @dataclass
 class InSequence(Language):
     ls: Tuple[Language, ...]
 
-    def push(self, x: Event):
-        pass  # XXX
-
 
 @dataclass
 class ZeroOrOne(Language):
     l: Language
-
-    def push(self, x: Event):
-        pass  # XXX
 
 
 @dataclass
 class ZeroOrMore(Language):
     l: Language
 
-    def push(self, x: Event):
-        pass  # XXX
-
 
 @dataclass
 class OneOrMore(Language):
     l: Language
-
-    def push(self, x: Event):
-        pass  # XXX
 
 
 @dataclass
 class Either(Language):
     ls: Tuple[Language, ...]
 
-    def push(self, x: Event):
-        pass  # XXX
-
-
 # Interaction protocol
+
 
 @dataclass
 class InteractionProtocol:
@@ -120,5 +78,4 @@ class InteractionProtocol:
     def __post_init__(self):
         from .language_parse import parse_language, language_to_str
         self.interaction = parse_language(self.language)
-        self.language= language_to_str(self.interaction)
-
+        self.language = language_to_str(self.interaction)
